ae9e1a9df5a90c69f0892a0691d5b0b647599574
emad <emadi68moh@gmail.com>
2025-03-08 22:21:55 +0330

    feat(core): add import address extraction utility for storybook setup

---
+++ lib/extractor/extractImportAddress.ts
@@ -0,0 +1,30 @@
+import path from "node:path";
+
+import { store } from "../store";
+
+export const extractImportAddress = (importPath: string, filePath: string) => {
+  if (importPath.startsWith("./")) {
+    const dir = filePath.split("/").slice(0, -1).join("/");
+    const relativeImportPath = path.join(dir, importPath);
+    return store.componentRelativePathToStoriesDirectory(relativeImportPath);
+  }
+
+  if (importPath.startsWith("../")) {
+    console.log("import local back", importPath);
+    const splittedPath = importPath.split("/");
+    const numberOfBacks = splittedPath.filter((item) => item === "..").length;
+    const pathWithoutBacks = splittedPath
+      .filter((item) => item !== "..")
+      .join("/");
+    const dir = filePath
+      .split("/")
+      .slice(0, -numberOfBacks - 1)
+      .join("/");
+
+    const relativeImportPath = path.join(dir, pathWithoutBacks);
+
+    return store.componentRelativePathToStoriesDirectory(relativeImportPath);
+  }
+
+  return importPath;
+};
