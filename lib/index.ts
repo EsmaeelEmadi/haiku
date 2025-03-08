44b582d09883e6b8ef4e370a3f2c873ec97f0030
emad <emadi68moh@gmail.com>
2025-03-09 00:06:29 +0330

    feat(core): add AST parsing for storybook creation
    This commit introduces AST parsing functionality to identify `$CreateStory` calls in source files and generate corresponding storybook files. It includes file discovery, AST traversal, and story execution logic.

--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 
-import chalk from "chalk";
-import ora from "ora";
-import { Command } from "commander";
-import { store } from "./store";
+import { executeCreateStory } from "./executeCreateStory";
 export { $CreateStory } from "./createStory";
+import fs from "node:fs";
+import path from "node:path";
+import chalk from "chalk";
+import { Command } from "commander";
+import ora from "ora";
+import { findCreateStoryCalls } from "./findCreateStoryCalls";
+import { findFiles } from "./findFiles";
+import { store } from "./store";
 import type { IGenenrateStorybook } from "./types";
 
 const program = new Command();
@@ -16,11 +21,13 @@ program
   .option("-o, --output-directory <type>", "output directory")
   .option("-s, --src-directory <type>", "source directory")
   .action(async (options: Partial<IGenenrateStorybook>) => {
+    let directoryPath: string | undefined;
+
     store.hostDirectory = process.cwd();
 
     // check arguments
-    { 
-      const spinner = ora("Check arguments").start(); 
+    {
+      const spinner = ora("Check arguments").start(); // Start the spinner
       if (!options.outputDirectory) {
         spinner.fail(chalk.red("Output directiry is required"));
         return;
@@ -29,6 +36,40 @@ program
       store.outputDirectory = options.outputDirectory;
 
       spinner.succeed(chalk.green("Incoming args are OK"));
+    }
+
+    {
+      directoryPath = path.join(store.hostDirectory, options.outputDirectory);
+
+      const spinner = ora("Check output path");
+
+      const stats = fs.statSync(directoryPath);
+
+      if (!stats.isDirectory()) {
+        fs.mkdirSync(directoryPath);
+      }
+      spinner.succeed(chalk.green("Check output path done"));
+    }
+
+    {
+      const sourcePath = options.srcDirectory
+        ? path.join(store.hostDirectory, options.srcDirectory)
+        : store.hostDirectory;
+
+      const files = findFiles(sourcePath, directoryPath);
+
+      const results = files
+        ?.filter((file) => file !== null)
+        .flatMap((file) => findCreateStoryCalls(file));
+
+      if (!results) {
+        chalk.yellow("unable to find any $CreateStory");
+        return;
+      }
+
+      for (const { filePath, sourceFile } of results) {
+        await executeCreateStory(filePath, sourceFile);
+      }
     }
   });
 
