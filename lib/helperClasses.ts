5570b707a75ded83c5330c813e1da9410ac4d05c
emad <emadi68moh@gmail.com>
2025-03-08 22:19:34 +0330

    feat(core): add helper classes for AST parsing and component extraction

---
+++ lib/helperClasses.ts
@@ -0,0 +1,33 @@
+export class Export {
+  constructor(
+    public readonly name: string,
+    public readonly path: string,
+    public readonly type: string,
+  ) {}
+
+  get pathWithoutExtention() {
+    return this.path.replace(/\.(ts|tsx)$/, "");
+  }
+}
+
+export class DefaultExport extends Export {}
+
+export class NamedExport extends Export {}
+
+export class DefaultImport extends Export {}
+
+export class NamedImport extends Export {}
+
+export class ExtractResult {
+  constructor(
+    readonly component: Export,
+    readonly props?: Record<string, unknown>,
+  ) {}
+}
+
+export class ImportDeclaration {
+  constructor(
+    public readonly name: string,
+    public readonly path: string,
+  ) {}
+}
